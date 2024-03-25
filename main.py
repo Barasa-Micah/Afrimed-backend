14e4cf22428ea294a366bed163dee00a300b5c32
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-03-25 15:48:04 +0300

    .

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.post('/login')
 async def login(username: str):
     return {"username": username}
+
+@app.post('/quizzes')
+async def quizes(game: str):
+    return {"username": game}
---
+++ quizes.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from fastapi import FastAPI 
+
+app = FastAPI()
+
+# class UserData(BaseModel):
+    # name: str 
+    # age: int 
+    # stress_level: str 
+    
+@app.get('/user/<username>')
+async def get_user(name:str, age:int, stress_level:str):
+    return {
+        "username": name, 
+        "age": age,
+        "stress_level": stress_level}  
