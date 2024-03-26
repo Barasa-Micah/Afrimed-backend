b175deeb50ef66831820c68ca103dc117aa3d973
Ian Bravyn <ianbravynsa@gmail.com>
2024-03-26 11:56:24 +0300

    set up api endpoint for medics using django rest

--- afrimed/afrimed/urls.py
+++ afrimed/afrimed/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
 urlpatterns = [
     path("patient/", include('patient.urls')),
     path('admin/', admin.site.urls),
+    path('api/', include('medicapi.urls'))
 ]
