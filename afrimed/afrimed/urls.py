941f8bb161a16cec3b76a025debcc1e1f1302a02
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-03-26 12:54:31 +0300

    .

--- afrimed/afrimed/urls.py
+++ afrimed/afrimed/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("patient/", include('patient.urls')),
+    # path("patient/", include('patient.urls')),
     path('api/', include('medicapi.urls'))
 ]
