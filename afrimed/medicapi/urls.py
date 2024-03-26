f90e9e6e9d1b8b30f9bd1615c8137b0239543e3f
Ian Bravyn <ianbravynsa@gmail.com>
2024-03-26 12:34:42 +0300

    .

---
+++ afrimed/medicapi/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('hello-medic/', views.hello_medics, name='hello_medic'),
+]
---
+++ afrimed/medicapi/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+# Create your views here.
+@api_view(['GET'])
+def hello_medics(request):
+    return Response({'message': 'Hello medics'})
+
+@api_view(['GET'])
+def view_users(request):
+    return Response({'name': 'user'})
