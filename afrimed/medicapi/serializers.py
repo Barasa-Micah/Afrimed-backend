4baf6ca4102124ec6027722f5a234eeda0504123
Ian Bravyn <ianbravynsa@gmail.com>
2024-03-29 23:59:46 +0300

    migrated tables

--- afrimed/medicapi/serializers.py
+++ afrimed/medicapi/serializers.py
@@ -4,4 +4,4 @@ from .models import Medics
 class MedicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Medics
-        fields = ["name", "medical_licence_number", "place_of_practice"]
+        fields = ["name", "medical_licence_number", "place_of_practice", "age"]
