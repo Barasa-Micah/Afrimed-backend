0d308e02785cf8adcac161f77665f2e71fbced7d
Ian Bravyn <ianbravynsa@gmail.com>
2024-03-30 10:42:15 +0300

    added gender field and migrated db

--- afrimed/medicapi/models.py
+++ afrimed/medicapi/models.py
@@ -3,11 +3,23 @@ from django.db import models
 # Create your models here.
 
 class Medics(models.Model):
+    
+    MALE = 'M'
+    FEMALE = 'F'
+    OTHER = 'O'
+
+    GENDER_CHOICES = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (OTHER, 'Other'),
+    ]
+    
     name = models.CharField(max_length = 200)
     medical_licence_number = models.IntegerField()
     place_of_practice = models.TextField()
     age = models.IntegerField()
-    gender = models.BinaryField("M", "F")
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES ) 
 
     def __str__(self) -> str:
         return self.name
+    
